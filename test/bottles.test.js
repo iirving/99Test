44c51f706c14d7ee856142692709bc48ed7e4616
Ian Irving <ian@falsepositives.com>
2023-10-31 16:42:56 -0400

    refactor verses

--- test/bottles.test.js
+++ test/bottles.test.js
@@ -89,22 +89,16 @@ describe("Bottles", () => {
 
   test("a few verses at the end", () => {
     const expected =
-      "2 bottles of beer on the wall, " +
-      "2 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "1 bottle of beer on the wall.\n" +
-      "\n" +
-      "1 bottle of beer on the wall, " +
-      "1 bottle of beer.\n" +
-      "Take it down and pass it around, " +
-      "no more bottles of beer on the wall.\n" +
-      "\n" +
-      "No more bottles of beer on the wall, " +
-      "no more bottles of beer.\n" +
-      "Go to the store and buy some more, " +
-      "99 bottles of beer on the wall.";
+      `2 bottles of beer on the wall, 2 bottles of beer.\n` +
+      `Take one down and pass it around, 1 bottle of beer on the wall.\n` +
+      `\n` +
+      `1 bottle of beer on the wall, 1 bottle of beer.\n` +
+      `Take it down and pass it around, no more bottles of beer on the wall.\n` +
+      `\n` +
+      `No more bottles of beer on the wall, no more bottles of beer.\n` +
+      `Go to the store and buy some more, 2 bottles of beer on the wall.\n`;
 
-    let result = new Bottles().verses(2, 0);
+    let result = new Bottles(2).verses();
 
     expect(result).toBe(expected);
   });
